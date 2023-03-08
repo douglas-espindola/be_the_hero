dd9d78ddeaaa771853bd6c0e74e417e97db7a4fc
Douglas Espindola <douglas.spindola15@gmail.com>
2023-03-08 20:58:57 -0300

    criado estrutura de pastas

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Hello OmniStack</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
